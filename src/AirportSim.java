a945911238e01759d8548860de9d3280f7fd63b2
hansenzhao <hansen305@gatech.edu>
2017-04-02 21:46:33 -0400

    multiple runway with comments

--- src/AirportSim.java
+++ src/AirportSim.java
@@ -12,19 +12,19 @@ public class AirportSim {
         // create 5 different airport
 
         Airport lax = new Airport("LAX",15,30,15, distance[0],
-                0, 0,0, 0, true, 4, 17);
+                0, 0,0, 0, true, 10, 17);
         Airport atl = new Airport("ATL",15,30,15, distance[1],
-                1,0,0,0, true, 5, 20);
+                1,0,0,0, true, 10, 20);
         Airport mia = new Airport("MIA",13,27,13, distance[2],
-                2,0,0,0, false, 4, 16);
+                2,0,0,0, false, 10, 16);
         Airport was = new Airport("WAS",12,25,12, distance[3],
-                3,0,0,0, false, 5, 15);
+                3,0,0,0, false, 10, 15);
         Airport sea = new Airport("SEA",13,27,13, distance[4],
-                4,0,0,0,true, 5, 15);
+                4,0,0,0,true, 10, 15);
         // put all the airport in one array
         Airport port[] = new Airport[]{lax, atl, mia, was, sea};
         // generate 16 airplanes
-        for(int i = 0; i < 90; i++) {
+        for(int i = 0; i < 8; i++) {
             Airplane airplane1 = new Airplane("A380", 0, 853, 587, i, true);
             Airplane airplane2 = new Airplane("B777", 0, 550, 558, i, false);
             AirportEvent landingEvent1 = new AirportEvent(0, port[i % 5], AirportEvent.PLANE_ARRIVES, airplane1, port, 0);
@@ -33,7 +33,7 @@ public class AirportSim {
             Simulator.schedule(landingEvent2);
         }
 
-        Simulator.stopAt(5000);
+        Simulator.stopAt(200);
         Simulator.run();
         // print the circling time depart people and arrive people
         for(int i = 0; i < port.length; i++){
